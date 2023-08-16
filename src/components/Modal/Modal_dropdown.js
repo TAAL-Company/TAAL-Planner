afa9fc97fb5974177c1bb668bb2d216cb62cd04e
Mohammad Obidat <97358018+Mohammad-Obidat@users.noreply.github.com>
2023-08-16 13:59:40 +0300

    Update Modal_dropdown.js

--- src/components/Modal/Modal_dropdown.js
+++ src/components/Modal/Modal_dropdown.js
@@ -1,60 +1,48 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Modal.css';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState, useEffect, useRef } from "react";
+import "./Modal.css";
 
 //--------------------------
 
 //--------------------------
 function Modal_dropdown(props) {
   const menuRef = useRef(null);
-  const [open, setOpen] = useState(false);
-  const myDiv = document.getElementById('dropdown');
 
   useEffect(() => {
-    if (props.setOpenThreeDotsVertical !== -1 && open) {
-      props.setOpenThreeDotsVertical(-1);
-    }
-
-    document.addEventListener('click', function (event) {
-      const targetElement = event.target;
-
-      if (menuRef.current && !menuRef.current.contains(targetElement)) {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
         props.setOpenThreeDotsVertical(-1);
       }
-    });
-  });
+    }
 
-  function Requestedit() {
-    setOpen(true);
-    props.setRequestForEditing('edit');
-  }
-
-  function Requestduplication() {
-    setOpen(true);
-    props.setRequestForEditing('duplication');
-  }
-
-  function Requestdetails() {
-    setOpen(true);
-    props.setRequestForEditing('details');
-  }
-
-  function Requestdelete() {
-    setOpen(true);
-    props.setRequestForEditing('delete');
-  }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div ref={menuRef}>
-      <div id='dropdown' className='button-dropdown-content'>
-        {props.editable ? <Link onClick={Requestedit}>עריכה</Link> : <></>}
-        {props.Reproducible ? (
-          <Link onClick={Requestduplication}>שכפול</Link>
+      <div id="dropdown" className="button-dropdown-content">
+        {props.editable ? (
+          <a onClick={() => props.setRequestForEditing("edit")}>עריכה</a>
         ) : (
           <></>
         )}
-        {props.details ? <Link onClick={Requestdetails}>פרטים</Link> : <></>}
-        {props.erasable ? <Link onClick={Requestdelete}>מחיקה</Link> : <></>}
+        {props.Reproducible ? (
+          <a onClick={() => props.setRequestForEditing("duplication")}>שכפול</a>
+        ) : (
+          <></>
+        )}
+        {props.details ? (
+          <a onClick={() => props.setRequestForEditing("details")}>פרטים</a>
+        ) : (
+          <></>
+        )}
+        {props.erasable ? (
+          <a onClick={() => props.setRequestForEditing("delete")}>מחיקה</a>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );

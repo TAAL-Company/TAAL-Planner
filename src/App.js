c40ee795139f17005bccb332663433ebb7f65ee3
Khalid idies <khalididies95@gmail.com>
2025-08-04 13:53:36 +0300

    fix: Correct import path for Calculator component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Planner from './components/Planner/Planner';
-import Calculator from './components/junk/Calculator/Calculator';
+import Calculator from './components/Calculator/Calculator';
 import Student from './components/Student/UsersTable';
 // import Student from '/components/Student/Student';
 import './App.css';
